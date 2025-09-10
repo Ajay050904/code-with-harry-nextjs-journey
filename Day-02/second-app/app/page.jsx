30c2b6f69d62b25779e822b0b247b51e84ee2a74
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-09-10 18:19:51 +0000

    Server Components in Next.js

--- Day-02/second-app/app/page.jsx
+++ Day-02/second-app/app/page.jsx
@@ -2,17 +2,19 @@
 // import { useEffect, useState } from "react";
 
 import fs from "fs/promises"
+import Navbar from "@/component/Navbar"
 
 export default function Home() {
 
   // const [count, setCount] = useState(0);
   // console.log("Hey I am ajay") // by default ye server SIDE  pe render hota h if we use "use client {At ToP} then its shows on clien server(user scrren cosnsole)"
-  
+
   let a = fs.readFile(".gitignore")
   a.then(e => { console.log(e.toString()) })
- 
+
   return (
     < div >
+      <Navbar />
       I am a component
       {/* {count} */}
       {/* <button onClick={() => setCount(count + 1)}> Click me</button> */}
