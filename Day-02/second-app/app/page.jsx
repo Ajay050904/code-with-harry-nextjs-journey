16c88c7f683691f28fddbc021661a9540dfec311
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-09-10 18:11:27 +0000

    4

--- Day-02/second-app/app/page.jsx
+++ Day-02/second-app/app/page.jsx
@@ -1,14 +1,21 @@
-"use client";
+// "use client";
+// import { useEffect, useState } from "react";
 
-import { useEffect, useState } from "react";
+import fs from "fs/promises"
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+
+  // const [count, setCount] = useState(0);
   // console.log("Hey I am ajay") // by default ye server SIDE  pe render hota h if we use "use client {At ToP} then its shows on clien server(user scrren cosnsole)"
+  
+  let a = fs.readFile(".gitignore")
+  a.then(e => { console.log(e.toString()) })
+ 
   return (
     < div >
-      I am a component {count}
-      <button onClick={() => setCount(count + 1)}> Click me</button>
+      I am a component
+      {/* {count} */}
+      {/* <button onClick={() => setCount(count + 1)}> Click me</button> */}
     </div >
   );
 }
