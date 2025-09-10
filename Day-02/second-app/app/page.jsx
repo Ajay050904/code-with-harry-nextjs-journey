bf2bf68c1d813f8a9c5e142cbb9e8dc7382f2c69
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-09-10 18:07:37 +0000

    3

--- Day-02/second-app/app/page.jsx
+++ Day-02/second-app/app/page.jsx
@@ -1,10 +1,14 @@
+"use client";
+
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
+  // console.log("Hey I am ajay") // by default ye server SIDE  pe render hota h if we use "use client {At ToP} then its shows on clien server(user scrren cosnsole)"
   return (
     < div >
       I am a component {count}
+      <button onClick={() => setCount(count + 1)}> Click me</button>
     </div >
   );
 }
