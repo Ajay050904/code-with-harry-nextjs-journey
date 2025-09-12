c608e330486dd09173c93cc0a6dc2be4cb4a3087
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-09-12 18:02:23 +0000

    4

--- Day-03/third-app/components/Footer.js
+++ Day-03/third-app/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='flex justify-around bg-slate-800 text-white py-4'>
+        <footer className='flex justify-around bg-slate-800 text-white py-4 text-xs'>
             <div className='text-center'>Copyroght Facebook © | All rights reserved</div>
             <ul className='flex gap-2 text-sm'>
                 <a href='/'><li className='text-xs'>Home</li></a>
--- Day-03/third-app/components/Navbar.js
+++ Day-03/third-app/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-around bg-slate-800 text-white py-4'>
+        <nav className='flex justify-between px-4 bg-slate-800 text-white py-4'>
             <div className="logo font-bold">Facebook</div>
             <ul className='flex gap-6'>
                 <a href='/'><li>Home</li></a>
