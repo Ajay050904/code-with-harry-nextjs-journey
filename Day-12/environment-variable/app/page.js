ee5424f09d2ce243d2c04887458aba7b1cd1e2a0
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-10-06 15:18:23 +0000

    3

--- Day-12/environment-variable/app/page.js
+++ Day-12/environment-variable/app/page.js
@@ -1,10 +1,16 @@
+ "use client"
 import Image from "next/image";
 
 export default function Home() {
-  console.log("The id is : ", process.env.ID);
-  console.log("The secret is : ", process.env.SECRET);
+  // console.log("The id is : ", process.env.ID);
+  // console.log("The secret is : ", process.env.SECRET);
   
   return (
     <div>HEY! This is home. The id is : {process.env.ID} and The secret is : { process.env.SECRET}</div>
+    // <div>HEY! This is home. The id is : {process.env.NEXT_PUBLIC_ID} and The secret is : { process.env.SECRET}</div>
   );
 }
+
+// procees.env -> server pe dono load hote h and accesable bhi h
+
+// usecleint krne per ab process.env ab client ki h n ki server ki and access nhi kr payega
