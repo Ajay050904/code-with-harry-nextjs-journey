6aa2f3c6b8b71623db695b98e1360cc5485c5c7e
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-10-06 15:29:10 +0000

    4

--- Day-12/environment-variable/app/page.js
+++ Day-12/environment-variable/app/page.js
@@ -1,4 +1,4 @@
- "use client"
+//  "use client"
 import Image from "next/image";
 
 export default function Home() {
@@ -6,8 +6,8 @@ export default function Home() {
   // console.log("The secret is : ", process.env.SECRET);
   
   return (
-    <div>HEY! This is home. The id is : {process.env.ID} and The secret is : { process.env.SECRET}</div>
-    // <div>HEY! This is home. The id is : {process.env.NEXT_PUBLIC_ID} and The secret is : { process.env.SECRET}</div>
+    // <div>HEY! This is home. The id is : {process.env.ID} and The secret is : { process.env.SECRET}</div>
+    <div>HEY! This is home. The id is : {process.env.NEXT_PUBLIC_ID} and The secret is : { process.env.NEXT_PUBLIC_SECRET} and name is {process.env.NAME}</div>
   );
 }
 
