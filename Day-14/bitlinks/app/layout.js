ab686eb3201095ed1fb63be8705f2737c5a5b722
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2025-10-10 15:23:24 +0000

    5

--- Day-14/bitlinks/app/layout.js
+++ Day-14/bitlinks/app/layout.js
@@ -1,15 +1,18 @@
+import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/component/Navbar";
 
-const geistSans = Geist({
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  weight: "100 900",
 });
 
-const geistMono = Geist_Mono({
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "100 900",
 });
 
 export const metadata = {
@@ -21,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-purple-50`}
       >
         <Navbar />
         {children}
